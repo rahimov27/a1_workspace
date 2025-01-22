fb2829f332548351f05cf7b78513f2cd9b3522b7
rahimov27 <89564054+rahimov27@users.noreply.github.com>
2025-01-22 21:12:34 +0100

    first push

---
+++ android/app/src/main/kotlin/com/example/a1_workspace/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.a1_workspace
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:a1_workspace/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+
