d1e1bc272340219259cb77f0c3f308420cc7e5fc
rahimov27 <89564054+rahimov27@users.noreply.github.com>
2025-02-26 23:47:08 +0100

    cleaned calendar page

---
+++ lib/features/calendar/data/models/meeting.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Meeting {
+  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
+
+  final String eventName;
+  final DateTime from;
+  final DateTime to;
+  final Color background;
+  final bool isAllDay;
+}
